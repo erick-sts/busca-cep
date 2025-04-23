353725cd3ff06da2b4eb9132f37a678bda3826bf
erick-sts <erick.sts.eo@gmail.com>
2025-04-22 21:35:52 -0300

    test

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ export default function App() {
           const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
           const data = await response.json();
           if (data.erro) {
-            showModal('CEP não encontrado');
+            showModal('CEP não encontrado!');
             setEndereco({ logradouro: '', bairro: '', localidade: '', uf: '' });
           } else {
             setEndereco({
